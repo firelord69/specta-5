78f3e731b4e8cbbeb2f533de8991193aa10a15c5
firelord69 <79193827+firelord69@users.noreply.github.com>
2021-11-21 10:24:06 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# specta-5
+# spectagram-stage-4
+project solution c84
---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCCGMUOlOHh7XfsxcwyqMIEZdH4_2R4KKI",
+    authDomain: "spectagram-3f2f8.firebaseapp.com",
+    projectId: "spectagram-3f2f8",
+    storageBucket: "spectagram-3f2f8.appspot.com",
+    messagingSenderId: "669561770011",
+    appId: "1:669561770011:web:7def519d91c271a75de8be"
+  };
